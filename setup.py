21e7cd0b753cc1686ac49599cb61f4e9ead45297
Bhargav Srinivasa <bhargavvader@gmail.com>
2020-01-10 15:02:03 -0600

    added IPython

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
                 'speechrecognition',
                 'pysoundfile',
                 'scikit-image',
+                'IPython'
                 'Pillow',
         ],
         classifiers = [
