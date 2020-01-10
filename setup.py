4c24aac153f228f36723d4b04936efdd337eb51a
Bhargav Srinivasa <bhargavvader@gmail.com>
2020-01-10 15:04:45 -0600

    update

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
                 'speechrecognition',
                 'pysoundfile',
                 'scikit-image',
-                'IPython'
+                'IPython',
                 'Pillow',
         ],
         classifiers = [
